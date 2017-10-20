b022a51b436c1aa878bcda213774bc4fb97912d1
wb-qcy313750 <wb-qcy313750@antfin.com>
2017-10-20 16:02:51 +0800

    lab04

---
+++ lab04/lab04_extra.py
@@ -0,0 +1,40 @@
+from lab04 import *
+
+# Q13
+
+
+def flatten(lst):
+    """Returns a flattened version of lst.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]      # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    """
+    if not lst:
+        return []
+    elif type(lst[0]) == list:
+        return flatten(lst[0]) + flatten(lst[1:])
+    else:
+        return [lst[0]] + flatten(lst[1:])
+
+# Q14
+
+
+def merge(lst1, lst2):
+    """Merges two sorted lists.
+
+    >>> merge([1, 3, 5], [2, 4, 6])
+    [1, 2, 3, 4, 5, 6]
+    >>> merge([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    """
+    return sorted((lst1 + lst2))
