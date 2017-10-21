ca168fbb8d87c892fd8e53c51b1843d9b583f81a
qzy <weelgod@gmail.com>
2017-10-21 08:59:34 +0800

    lab03

---
+++ Functions/recursion.py
@@ -0,0 +1,65 @@
+def split(n):
+    return n // 10, n % 10
+
+
+def sum_digits(n):
+    if n < 10:
+        return n
+    else:
+        all_but_last, last = split(n)
+        return sum_digits(all_but_last) + last
+
+
+def luhn_sum(n):
+    if n < 10:
+        return n
+    else:
+        all_but_last, last = split(n)
+        return luhn_sum_double(all_but_last) + last
+
+
+def luhn_sum_double(n):
+    all_but_last, last = split(n)
+    luhn_digit = sum_digits(2 * last)
+    if n < 10:
+        return luhn_digit
+    else:
+        return luhn_sum(all_but_last) + luhn_digit
+
+
+def cascade(n):
+    print(n)
+    if n >= 10:
+        cascade(n // 10)
+        print(n)
+
+
+def inverse_cascade(n):
+    grow(n)
+    print(n)
+    shrink(n)
+
+
+def f_then_g(f, g, n):
+    if n:
+        f(n)
+        g(n)
+
+
+def grow(n): return f_then_g(grow, print, n // 10)
+
+
+def shrink(n): return f_then_g(print, shrink, n // 10)
+
+
+from ucb import trace
+
+
+@trace
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 2) + fib(n - 1)
