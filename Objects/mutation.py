c0c853cb1f1cd191e936c431020e8b7c3c09ae26
wb-qcy313750 <wb-qcy313750@antfin.com>
2017-12-14 10:11:54 +0800

    scheme

---
+++ Objects/mutation.py
@@ -0,0 +1,43 @@
+def swap(a, b):
+    """Swap the contents of lists a and b.
+
+    >>> a = [1, 'two', 3]
+    >>> b = [4, [5, 6]]
+    >>> swap(a, b)
+    >>> a
+    [4, [5, 6]]
+    >>> b
+    [1, 'two', 3]
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def make_accumulator():
+    """Return an accumulator function that takes a single numeric argument and
+    accumulates that argument into total, then returns total.
+
+    >>> acc = make_accumulator()
+    >>> acc(15)
+    15
+    >>> acc(10)
+    25
+    >>> acc2 = make_accumulator()
+    >>> acc2(7)
+    7
+    >>> acc3 = acc2
+    >>> acc3(6)
+    13
+    >>> acc2(5)
+    18
+    >>> acc(4)
+    29
+    """
+
+    count = 0
+
+    def accumulator(n):
+        nonlocal count
+        count += n
+        return count
+
+    return accumulator
